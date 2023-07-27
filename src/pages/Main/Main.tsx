2630aa23cde84a05a1fc733b794a7cbc7bc42316
gr1fon4ik <hekjd12@gmail.com>
2023-07-27 21:52:21 +0300

    fix main page descr

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -17,7 +17,7 @@ const Main = () => {
       <PreLoader />
       <MyHelmet
         title="ROSAMUNDI"
-        description="rosamundi — поэтический проект, вдохновленный философией Д. Андреева."
+        description="rosamundi — поэтический проект, вдохновленный философией Д. Андреева. Здесь вы можете ознакомиться с творениями современных поэтов."
         image="https://rosa-mundi.ru/og.jpg"
       />
       <Template backgroundImage="main" footer={true} header={true} headerColor={false}>
